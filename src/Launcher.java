3dbab7288657d305884f0a91ad1a03f2ca716ade
Baptiste <baptiste.bucamp@epita.fr>
2022-02-10 12:39:50 +0100

    part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -10,6 +10,30 @@ public class Launcher {
             String txt = scanner.nextLine();
             if (txt.equals("quit"))
                 System.exit(0);
+            if (txt.equals("fibo"))
+            {
+                System.out.println("Give the rank n of the fibonacci sequence");
+                int n = scanner.nextInt();
+                String s = scanner.nextLine();
+                if (n == 1)
+                {
+                    System.out.println("1");
+                }
+                else
+                {
+                    long n1 = 0;
+                    long n2 = 1;
+                    long res = 0;
+                    while (n >= 2)
+                    {
+                        res = n1 + n2;
+                        n1 = n2;
+                        n2 = res;
+                        n-=1;
+                    }
+                    System.out.println(res);
+                }
+            }
             else
                 System.out.println("Unknown command");
         }
