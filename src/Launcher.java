8a0bb81e2c307fa7628af59352c3a461eff50c48
Baptiste <baptiste.bucamp@epita.fr>
2022-02-11 18:40:01 +0100

    part 6

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,77 +1,28 @@
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.*;
 
 public class Launcher {
     public static void main(String[] args)
     {
+        List<Command> commands = new ArrayList<Command>();
+        commands.add(new Freq());
+        commands.add(new Fibo());
+        commands.add(new Quit());
         System.out.println("Welcome !");
         Scanner scanner = new Scanner(System.in);
-        while (true)
+        boolean test = true;
+        while (test)
         {
             String txt = scanner.nextLine();
-            if (txt.equals("quit"))
-                System.exit(0);
-            if (txt.equals("fibo"))
-            {
-                System.out.println("Give the rank n of the fibonacci sequence");
-                int n = scanner.nextInt();
-                String s = scanner.nextLine();
-                if (n == 1)
-                {
-                    System.out.println("1");
-                }
-                else
-                {
-                    long n1 = 0;
-                    long n2 = 1;
-                    long res = 0;
-                    while (n >= 2)
-                    {
-                        res = n1 + n2;
-                        n1 = n2;
-                        n2 = res;
-                        n-=1;
-                    }
-                    System.out.println(res);
-                }
-            }
-            if (txt.equals("freq"))
-            {
-                System.out.println("Give the path of the file");
-                String s = scanner.nextLine();
-                Map<String, Integer> map = new HashMap<String, Integer>();
-                try
-                {
-                    Scanner txtFile = new Scanner(new File(s));
-
-                    while (txtFile.hasNext())
-                    {
-                        String word = txtFile.next().toLowerCase().replaceAll("[,!?;'\n.]", " ");
-                        if (map.containsKey(word))
-                        {
-                            int count = map.get(word) + 1;
-                            map.put(word, count);
-                        }
-                        else
-                        {
-                            map.put(word, 1);
-                        }
-                    }
-                    txtFile.close();
-                }catch (FileNotFoundException e)
-                {
-                    System.out.println("Unreadable file: ");
-                }
-                //map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3).forEachOrdered(System.out::print);
-                map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3).forEachOrdered(entry -> {System.out.print(entry.getKey() + " "); });
-                System.out.println();
-            }
-            else
+            Command comm = commands.stream().filter(writtenCMD -> txt.equals(writtenCMD.name())).findAny()
+                .orElse(null);
+            if (Objects.isNull(comm)) {
                 System.out.println("Unknown command");
+            } else {
+                test = comm.run(scanner);
+            }
         }
+        scanner.close();
     }
 }
