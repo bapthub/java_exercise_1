bc9bb058c4208f9c92c3f52d1d651ff0b1944895
Baptiste <baptiste.bucamp@epita.fr>
2022-02-11 16:23:51 +0100

    step 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
 
 public class Launcher {
     public static void main(String[] args)
@@ -33,6 +38,36 @@ public class Launcher {
                     }
                     System.out.println(res);
                 }
+            }
+            if (txt.equals("freq"))
+            {
+                System.out.println("Give the path of the file");
+                String s = scanner.nextLine();
+                Map<String, Integer> map = new HashMap<String, Integer>();
+                try
+                {
+                    Scanner txtFile = new Scanner(new File(s));
+                    while (txtFile.hasNext())
+                    {
+                        String word = txtFile.next();
+                        if (map.containsKey(word))
+                        {
+                            int count = map.get(word) + 1;
+                            map.put(word, count);
+                        }
+                        else
+                        {
+                            map.put(word, 1);
+                        }
+                    }
+                    txtFile.close();
+                }catch (FileNotFoundException e)
+                {
+                    System.out.println("Unreadable file: ");
+                }
+                //map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3).forEachOrdered(System.out::print);
+                map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3).forEachOrdered(entry -> {System.out.print(entry.getKey() + " "); });
+                System.out.println();
             }
             else
                 System.out.println("Unknown command");
